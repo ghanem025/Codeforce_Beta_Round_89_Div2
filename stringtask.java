230fe5df06abbf4f49e7d9781bdbbc082a2e754c
Ghanem <40302667+ghanem025@users.noreply.github.com>
2021-03-06 23:38:35 -0500

    problem A solution

---
+++ stringtask.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.util.Scanner;
+import java.lang.String;
+import java.io.BufferedInputStream;
+public class stringtask{
+  public static void main(String [] args){
+    Scanner sc = new Scanner(System.in);
+    String word = sc.nextLine();
+    word = word.toLowerCase();
+ 
+    word= word.replaceAll("a","");
+    word = word.replaceAll("e","");
+    word = word.replaceAll("o","");
+    word = word.replaceAll("y","");
+    word = word.replaceAll("u","");
+    word = word.replaceAll("i","");
+ 
+    String result="";
+    for(int i=0; i<word.length();i++){
+      result=result+'.'+word.charAt(i);
+      }
+    System.out.println(result);
+    }
+  }
